7243876c8f757e38c7550ea5fcf16af6dec327ef
Zanardi <gustavo.zanardin@gmail.com>
2022-05-04 22:02:03 -0300

    Criação de classes para gerenciamento de estudo, topicos e perguntas

---
+++ PIGrupo03/src/modelo/Estudo.java
@@ -0,0 +1,12 @@
+package modelo;
+
+import java.util.ArrayList;
+
+public class Estudo {
+	
+	ArrayList<Topico> filaEstudo;
+	
+	Estudo(){
+		
+	}
+}
---
+++ PIGrupo03/src/modelo/Topico.java
@@ -0,0 +1,17 @@
+package modelo;
+
+public class Topico {
+	
+	private int codigo;
+	private int proficiencia;
+	private int posicao;
+	
+	private String titulo;
+	private String explicacao;
+	
+	Pergunta pergunta;
+	
+	Topico(int codigo){
+		this.codigo = codigo;
+	}
+}
